fa6d2cd249df4a1582b2c0dc7dcee629fd3d92c7
Thommy Jakobsson <thommyj@gmail.com>
2018-04-02 00:06:39 +0200

    move html-files into own directory
    Signed-off-by: Thommy Jakobsson <thommyj@gmail.com>

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var sqlite3 = require('sqlite3');
 var nodestatic = require('node-static');
 
 // Setup static server for current directory
-var staticServer = new nodestatic.Server(".");
+var staticServer = new nodestatic.Server("./www");
 
 // Setup database connection for logging
 var db = new sqlite3.Database('./piTemps.db');
@@ -137,8 +137,6 @@ var server = http.createServer( function(request, response) {
         response.writeHead(200, {'Content-Type': 'image/x-icon'});
         response.end();
 
-        // Optionally log favicon requests.
-        //console.log('favicon requested');
         return;
     } else {
         // Print requested file to terminal
